63f7fbfd07bee899952820a7ba3f73d6af41a80b
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-04-09 08:15:38 +0400

    Add files via upload

---
+++ newlaserbeam_alarm.ino
@@ -0,0 +1,35 @@
+#define LDR A0// connect LDR to Pin A0 of Arduino
+#define LED 8// connect LED to Pin 8 of Arduino
+#define Buzzer 7// connect Buzzer to Pin 7 of Arduino
+float x;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode (LED,OUTPUT);
+  pinMode (Buzzer,OUTPUT);
+}
+void loop()
+{
+  x=analogRead(A0);
+  x=x/10;
+  Serial.print("Value");
+  Serial.println(x);
+  if (x<75)
+  {
+  for(int i=0;i<10;i++)
+    digitalWrite(LED,HIGH);
+    Serial.print("DARK");
+    tone(Buzzer,1000);
+    delay(500);
+    tone(Buzzer,500);
+    delay(500);
+  
+  }
+  else
+  {
+   digitalWrite(LED,LOW); 
+   Serial.print("LIGHT");
+   noTone(Buzzer);
+  }
+  delay(100);
+}
